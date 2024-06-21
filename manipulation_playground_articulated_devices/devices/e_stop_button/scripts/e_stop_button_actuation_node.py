d8747962909b726a8e1fb3f9eba0b78366b49b60
JacquesCloete <jacques.cloete@trinity.ox.ac.uk>
2024-06-21 23:06:17 +0100

    Rename topic

--- manipulation_playground_articulated_devices/devices/e_stop_button/scripts/e_stop_button_actuation_node.py
+++ manipulation_playground_articulated_devices/devices/e_stop_button/scripts/e_stop_button_actuation_node.py
@@ -5,23 +5,23 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Bool, Float64
 
 
-bool_pub = rospy.Publisher('activated', Bool, queue_size=1)
+bool_pub = rospy.Publisher('activation_state', Bool, queue_size=1)
 command_pub = rospy.Publisher('e_stop_button_controller/command', Float64, queue_size = 10)
 
-activated = Bool(False)
+activation_state = Bool(False)
 joint_target = Float64()
 
 def callback(joint_states):
-    if (joint_states.position[0] < -0.0045) and (joint_states.position[1] > -1.047) and (not activated.data):
-        activated.data = True
+    if (joint_states.position[0] < -0.0045) and (joint_states.position[1] > -1.047) and (not activation_state.data):
+        activation_state.data = True
         joint_target.data = -0.005
         command_pub.publish(joint_target)
-    elif (joint_states.position[0] < -0.0045) and (joint_states.position[1] < -1.047) and (activated.data):
-        activated.data = False
+    elif (joint_states.position[0] < -0.0045) and (joint_states.position[1] < -1.047) and (activation_state.data):
+        activation_state.data = False
         joint_target.data = 0.0
         command_pub.publish(joint_target)
 
-    bool_pub.publish(activated)
+    bool_pub.publish(activation_state)
     
 
 if __name__ == '__main__':
